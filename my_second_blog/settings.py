d2b8c3d707c74900c3886a771bad43efbbfc3bc7
Pavlo Schutskiy <pavlo050706@gmail.com>
2025-10-15 18:04:21 +0300

    Resolved security issues, added base index page for polls app.

--- my_second_blog/settings.py
+++ my_second_blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
+
+# Load environmental variables
+config = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ahz@8@@_3k2cdch+7%%1&fvc!un3!5a$7-@o^m8eakhedmbxt*'
+SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
