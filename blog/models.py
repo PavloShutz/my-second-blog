9aa2b0fe0dac74717a9a1eea528c890c5d355c31
Pavlo Schutskiy <pavlo050706@gmail.com>
2025-10-19 17:07:02 +0300

    Blog application

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Text, title, author, likes, dislikes
+# Published date, created date, edited date
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_text = models.TextField(help_text="Here comes your magic...")
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField("date published")
+    cr_date = models.DateTimeField("date created")
+    edit_date = models.DateTimeField("date edited")
+
+    def __str__(self):
+        return self.post_title
+
+
+# Text, author, likes, dislikes
+# Published date, edited date
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField(help_text="Here comes your magic...")
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField("date published")
+    edit_date = models.DateTimeField("date edited")
