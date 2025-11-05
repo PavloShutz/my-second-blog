1c048855c5662a58fb01bc576be3231f0acab385
Pavlo Shutz <pavlo050706@gmail.com>
2025-11-05 23:19:21 +0200

    Path for uploaded media

--- my_second_blog/urls.py
+++ my_second_blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include("polls.urls")),
     path('blog/', include("blog.urls")),
-] + debug_toolbar_urls()
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
